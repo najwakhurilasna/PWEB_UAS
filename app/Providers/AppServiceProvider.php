0f2bfa4dd6e9fdac1637a6dd2182820279b76333
najwakhuril@outlook.com <najwakhuril@outlook.com>
2026-06-09 14:45:04 +0700

    Fix deployment issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Force HTTPS di production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Untuk database yang lebih lama
+        Schema::defaultStringLength(191);
     }
 }
